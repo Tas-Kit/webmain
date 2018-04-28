1fcf812783c43780862398435f7fed52132aafcd
Tianhang Liu <tianhanl@usc.edu>
2018-04-28 15:02:20 -0700

    add side task nav

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(path.join(__dirname, '/build')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.resolve(__dirname, '/build/index.html'));
 });
 
 app.listen(app.get('port'), () => {
---
+++ src/components/TaskPanel.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import { withStyles } from 'material-ui/styles';
+import List, { ListItem, ListItemText, ListItemIcon } from 'material-ui/List';
+import grey from 'material-ui/colors/grey';
+import Grid from 'material-ui/Grid';
+import Avatar from 'material-ui/Avatar';
+import TaskIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import NotificationIcon from '@material-ui/icons/Notifications';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  avatar: {},
+  row: {
+    padding: '12px 24px'
+  },
+  taskDrawer: {
+    position: 'relative',
+    paddingTop: '1em',
+    color: 'white',
+    background: grey[800],
+    width: drawerWidth
+  },
+  taskListItemText: {
+    color: 'white',
+    fontWidth: '700'
+  },
+  taskListItemIcon: {
+    color: grey[400]
+  },
+  NotificationIcon: {
+    transform: 'scale(1.5)'
+  }
+});
+
+const TaskPanel = ({ tasks, handleTaskClick, classes }) => {
+  return (
+    <Drawer
+      classes={{
+        paper: classes.taskDrawer
+      }}
+      variant="permanent"
+      anchor={'left'}
+    >
+      <Grid
+        container
+        justify={'space-between'}
+        alignItems={'center'}
+        className={classes.row}
+      >
+        <Avatar className={classes.avatar}>YZ</Avatar>
+        <NotificationIcon className={classes.NotificationIcon} />
+      </Grid>
+
+      <List>
+        {tasks.map(task => (
+          <ListItem key={task.taskId}>
+            <ListItemIcon className={classes.taskListItemIcon}>
+              <TaskIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={task.task.name}
+              classes={{
+                primary: classes.taskListItemText
+              }}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default withStyles(styles)(TaskPanel);
