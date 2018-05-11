e52786da471349435b0f647908b8386b6e92e720
Tianhang Liu <tianhanl@usc.edu>
2018-05-10 20:43:45 -0700

    modify api.js as eslint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
-var baseUrl = 'http://localhost:8001/api/v1';
+let baseUrl = 'http://localhost:8001/api/v1';
 
 if (typeof window !== 'undefined') {
-  var location = window.location;
-  baseUrl = location.protocol + '//' + location.host + '/api/v1'; // (or whatever)
+  const { protocol, host } = window.location;
+  baseUrl = `${protocol}//${host}/api/v1`; // (or whatever)
 }
 
 const handleTimeOut = () => {
@@ -19,23 +19,22 @@ const transformResponse = (res, onError = defaultOnError) => {
   console.log(res);
   if (res.ok) {
     return res.json();
-  } else {
-    switch (res.status) {
-      case 401:
-        handleTimeOut();
-        break;
-      default:
-        onError(res);
-    }
+  }
+  switch (res.status) {
+    case 401:
+      handleTimeOut();
+      break;
+    default:
+      onError(res);
   }
 };
 
 export const fetchTasks = () => {
   return fetch(`${baseUrl}/task/?format=json`, {
     headers: {
-      Accept: 'application/json'
+      Accept: 'application/json',
     },
-    credentials: 'include'
+    credentials: 'include',
   }).then(res => {
     return transformResponse(res);
   });
@@ -44,9 +43,9 @@ export const fetchTasks = () => {
 export const fetchTaskGraph = taskId => {
   return fetch(`${baseUrl}/task/graph/${taskId}`, {
     headers: {
-      Accept: 'application/json'
+      Accept: 'application/json',
     },
-    credentials: 'include'
+    credentials: 'include',
   }).then(res => {
     return transformResponse(res);
   });
@@ -54,5 +53,5 @@ export const fetchTaskGraph = taskId => {
 
 export default {
   fetchTasks,
-  fetchTaskGraph
+  fetchTaskGraph,
 };
