551e1f6b0f0f4639f162828ab19d08a13048d708
Tianhang Liu <tianhanl@usc.edu>
2018-05-14 20:32:00 +0800

    setup auto locale provider

---
+++ src/components/AutoIntlProvider.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+// polyfill for safari
+import intl from 'intl';
+
+// import locale libraries
+import zh from '../locales/zh';
+import en from '../locales/en';
+
+const languageMap = {
+  zh,
+  en,
+};
+
+const detectLocale = () => {
+  if (navigator) {
+    return navigator.language.split('_')[0];
+  }
+  return 'en';
+};
+
+
+const AutoIntlProvider = props => {
+  const { children } = props;
+
+  return (
+    <IntlProvider
+      locale={detectLocale()}
+      messages={languageMap[detectLocale()]}
+    >
+      {children}
+    </IntlProvider>
+  );
+};
+
+export default AutoIntlProvider;
