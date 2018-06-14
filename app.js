0eaa641fbb02d57b255a5deeb854667ae58cf5a8
Tianhang Liu <tianhanl@usc.edu>
2018-06-15 00:15:43 +0800

    solve problem caused by not recognizing static files

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+  req.url = req.url.replace('/web/main', '');
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname, '/build')));
+
+app.use(express.static(path.join(__dirname, '/build/static')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/index.html'));
