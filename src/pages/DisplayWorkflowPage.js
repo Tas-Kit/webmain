9d965fb69f011b87fe54612d08613934f71486b0
Yiyang Li <704493974@qq.com>
2018-08-13 23:27:04 +0800

    create pure viewer for display workflow purpose

--- src/pages/DisplayWorkflowPage.js
+++ src/pages/DisplayWorkflowPage.js
@@ -1,20 +1,104 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
 import { FormattedMessage } from 'react-intl';
-import Button from '@material-ui/core/Button';
+
+import { LoadingButton } from '../components/Button';
+
+// redux actions
+import * as taskActions from '../actions/taskActions';
+import * as currentUserActions from '../actions/currentUserActions';
+import * as snackbarActions from '../actions/snackbarActions';
+
+// containers
+import SnackbarContainer from '../containers/SnackbarContainer';
+import PureGraphViewerContainer from '../containers/PureGraphViewerContainer';
+
+// services
+import APIService from '../services/APIService';
+
+// constants
+import * as apiTypes from '../constants/apiTypes';
+import { TASK_GRAPH_URL } from '../constants/apiUrls';
 
 const inline = {
-  instructions: {
+  buttonDiv: {
     marginLeft: 20,
-    marginTop: 20,
+    marginRight: 20,
   },
 };
 
-const DisplayWorkflowPage = () => (
-  <div style={inline.instructions}>
-    <Button variant="outlined" color="primary">
-      <FormattedMessage id="displayWorkflow" defaultMessage="" />
-    </Button>
-  </div>
-);
+const styles = () => ({
+  chip: {
+    margin: '0px 2.5px',
+  },
+  trigger: {
+    margin: '10px 0px',
+  },
+});
 
-export default DisplayWorkflowPage;
+class DisplayWorkflowPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      displayed: false,
+    };
+  }
+
+  sendRequest = (taskId) => {
+    const url = `${TASK_GRAPH_URL}${taskId}/`;
+    APIService.sendRequest(url, apiTypes.GET_TASK_GRAPH)
+      .then((success) => {
+        if (success) {
+          this.setState({ displayed: true });
+        } else {
+          const { updateMessage, toggleTaskActionPending } = this.props.actions;
+          toggleTaskActionPending();
+          updateMessage(<FormattedMessage id="noTaskFoundMsg" />);
+        }
+      });
+  }
+
+  handleDisplayGraphClick = () => {
+    const { taskId } = this.props.match.params;
+    this.props.actions.setActiveTaskId(taskId);
+    this.sendRequest(taskId);
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { pending } = this.props.taskManager;
+    const { displayed } = this.state;
+    return (
+      <div>
+        {!displayed &&
+          <div style={inline.buttonDiv}>
+            <LoadingButton
+              variant="outlined"
+              color="primary"
+              className={classes.trigger}
+              buttonName={<FormattedMessage id="displayWorkflow" defaultMessage="" />}
+              loading={pending}
+              onClick={this.handleDisplayGraphClick}
+            />
+          </div>
+        }
+
+        {/* Pure Graph Viewer */}
+        {displayed && <PureGraphViewerContainer />}
+
+        {/* Snack Bar */}
+        <SnackbarContainer />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ taskManager }) => ({ taskManager });
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ ...taskActions, ...currentUserActions, ...snackbarActions }, dispatch),
+});
+
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(DisplayWorkflowPage));
