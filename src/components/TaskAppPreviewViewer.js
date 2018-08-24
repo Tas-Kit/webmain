f2086e7cd023e918953c9cbbf6d4f235315c874a
Tianhang Liu <tianhanl@usc.edu>
2018-08-23 22:26:54 -0700

    add download button in preview

--- src/components/TaskAppPreviewViewer.js
+++ src/components/TaskAppPreviewViewer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Button } from '@material-ui/core';
 import { STATUS_MAP_TWO, TIME_UNITS_MAP_TWO } from '../constants';
 
 const TaskAppPreviewViewer = (props) => {
-  const { task } = props;
+  const { task, handleDownloadClick } = props;
   if (!task) return (<p><FormattedMessage id="taskUnavailableText" /></p>);
   return (
     <div>
@@ -12,6 +13,7 @@ const TaskAppPreviewViewer = (props) => {
       <p><FormattedMessage id="statusFieldName" />: {STATUS_MAP_TWO[task.status]}</p>
       <p><FormattedMessage id="rolesFieldName" />: {task.roles.join(' ')}</p>
       <p><FormattedMessage id="expectedFieldName" />: {task.expected_effort_unit ? `${task.expected_effort_num} ${TIME_UNITS_MAP_TWO[task.expected_effort_unit]}` : <FormattedMessage id="noneText" />}</p>
+      <Button color="primary" size="small" onClick={handleDownloadClick} ><FormattedMessage id="downloadButton" /></Button>
     </div>
   );
 };
